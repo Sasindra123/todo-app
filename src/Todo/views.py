928ed318992cdede0c9d78d84a3e70fdce28901a
Sasindra123 <durgasasindra@gmail.com>
2024-05-27 20:25:47 +0530

    Add task Functionality

--- src/Todo/views.py
+++ src/Todo/views.py
@@ -1,5 +1,11 @@
 from django.http import *
 from django.shortcuts import render
+from todos.models import *
 
 def home(request):
-  return render(request,'home.html')
+  tasks = Task.objects.filter(is_completed=False).order_by('updated_at') # here we need taks that are not completed
+  print(tasks)
+  context={
+    'tasks':tasks,
+  }
+  return render(request,'home.html',context)
