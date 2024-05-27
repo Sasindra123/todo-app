8ca4efc62b37087665aef43864429ab91c5d67c8
Sasindra123 <durgasasindra@gmail.com>
2024-05-27 23:52:48 +0530

    Final Intialization

--- src/Todo/views.py
+++ src/Todo/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from todos.models import *
 
 def home(request):
-  tasks = Task.objects.filter(is_completed=False).order_by('updated_at') # here we need taks that are not completed
-  print(tasks)
+  tasks = Task.objects.filter(is_completed=False).order_by('-updated_at') # here we need taks that are not completed
+  completed_tasks=Task.objects.filter(is_completed=True)
+
   context={
     'tasks':tasks,
+    'completed_tasks':completed_tasks
   }
   return render(request,'home.html',context)
--- src/todos/urls.py
+++ src/todos/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
   path('addTask/',views.addTask,name='addTask'),
+  path('markComplete/<int:pk>/',views.markComplete,name='markComplete'),
+  path('markUncomplete/<int:pk>/',views.markUncomplete,name='markUncomplete'),
+  path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+  path('delete_task/<int:pk>/',views.delete_task,name='delete_task'),
 ]
